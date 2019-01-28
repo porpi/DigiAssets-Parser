3072c8f63bef6d4c87d4032f2e1b6c8c8ca0c2c0
Gary <esot@digibyte.io>
2019-01-29 08:51:58 +1000

    Update address length

--- models/coloredCoinsDataTypes.js
+++ models/coloredCoinsDataTypes.js
@@ -2,6 +2,6 @@ var Sequelize = require('sequelize')
 
 module.exports = {
 	HASH: Sequelize.STRING(64),	// even though this could be of type character (n), there is no performance advantage for this type. In fact, its the slowest char type.
-	ADDRESS: Sequelize.STRING(35),
+	ADDRESS: Sequelize.STRING(64),
 	ASSETID: Sequelize.STRING(40)
 }
